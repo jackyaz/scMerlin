ea5b08c0a6f77a53a25be995e11602f94e68bfdf
Jack <jack.naisbett@gmail.com>
2019-04-21 12:40:00 +0100

    More formatting

--- scmerlin.sh
+++ scmerlin.sh
@@ -208,7 +208,7 @@ MainMenu(){
 	printf "e.    Exit %s\\n\\n" "$SCM_NAME"
 	printf "z.    Uninstall %s\\n" "$SCM_NAME"
 	printf "\\n"
-	printf "\\e[1m##########################################################\\e[0m\\n"
+	printf "\\e[1m#####################################################\\e[0m\\n"
 	printf "\\n"
 	
 	while true; do
