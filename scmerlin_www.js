90d4d4571548851bbe8d7994a9915a0f7465f5af
Jack Naisbett <jack.naisbett@gmail.com>
2021-03-30 21:01:39 +0100

    Fix collapsible headers

--- scmerlin_www.js
+++ scmerlin_www.js
@@ -291,7 +291,7 @@ function SetCookie(cookiename,cookievalue){
 }
 
 function AddEventHandlers(){
-	$j(".collapsible-jquery").click(function(){
+	$j(".collapsible-jquery").off('click').on('click', function(){
 		$j(this).siblings().toggle("fast",function(){
 			if($j(this).css("display") == "none"){
 				SetCookie($j(this).siblings()[0].id,"collapsed");
