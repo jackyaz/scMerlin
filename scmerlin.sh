6cfcf4b5bbe746553f1e748fc4ad2f3a99f3475b
Jack Naisbett <jack.naisbett@gmail.com>
2021-04-05 20:22:32 +0100

    Version bump

--- scmerlin.sh
+++ scmerlin.sh
@@ -22,8 +22,8 @@
 ### Start of script variables ###
 readonly SCRIPT_NAME="scMerlin"
 readonly SCRIPT_NAME_LOWER=$(echo $SCRIPT_NAME | tr 'A-Z' 'a-z' | sed 's/d//')
-readonly SCM_VERSION="v2.2.1"
-readonly SCRIPT_VERSION="v2.2.1"
+readonly SCM_VERSION="v2.2.2"
+readonly SCRIPT_VERSION="v2.2.2"
 SCRIPT_BRANCH="master"
 SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME_LOWER.d"
